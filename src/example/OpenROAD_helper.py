e82afc9c1626a359c06a138d931c64a47acef30c
Bing-Yue Wu <bywu@asu.edu>
2024-05-17 12:37:51 -0700

    set ideal clock signal for the benchmarks
    Signed-off-by: Bing-Yue Wu <bywu@asu.edu>

--- src/example/OpenROAD_helper.py
+++ src/example/OpenROAD_helper.py
@@ -61,7 +61,6 @@ def load_design(design_name, verilog = False):
   design.evalTclString("read_spef %s"%spefFile)
   sdcFile = "%s/%s.sdc"%(designDir.as_posix(), design_name)
   design.evalTclString("read_sdc %s"%sdcFile)
-  design.evalTclString("set_propagated_clock [all_clocks]")
   design.evalTclString("source ../../platform/ASAP7/setRC.tcl")
   
   # Global connect
